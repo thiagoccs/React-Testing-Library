9e9d3211afe32d28a7a9ee7a453b84e98582755b
Thiago Souza <thiago29.06@hotmail.com>
2022-09-20 16:18:32 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('requisito 3', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const text = screen.getByText(/No favorite pokemon found/i);
+
+    expect(text).toBeInTheDocument();
+
+    if (!text) {
+      const pokemons = querySelector('#root > div > div > div:nth-child(2) > div');
+
+      expect(pokemons).toBeInTheDocument();
+    }
+  });
+});
